e6512881144f259c818f2cb1b0ccb9db4082fab9
Honeyposh <odedoyinoyinloluwa@gmail.com>
2023-09-13 15:49:21 +0100

    update port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const userRoute = require("./routes/userRoute");
 const mongoose = require("mongoose");
 const errorRoute = require("./controllers/notFound");
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRoute);
 app.use(errorRoute.notFound);
@@ -12,6 +13,5 @@ mongoose
     `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.1nqrgaa.mongodb.net/Second-Task?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log("connected");
-    app.listen(3000);
+    app.listen(PORT);
   });
